2a288718486735f6a8ee12e01cf669b11943a030
Valentine124 <kaluvalentine60@gmail.com>
2023-02-16 11:34:27 -0800

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 
+/**
+ * Main method is the program entry point
+ * it will always return 0
+ * and 0 means success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
