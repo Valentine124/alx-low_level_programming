bf67c9523ffd3cb767cc160ee5dcbf9ace4aa07a
Valentine124 <kaluvalentine60@gmail.com>
2023-02-16 18:42:28 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        write(STDERR_FILENO, message, 41);
-        return 1;
+        write(STDERR_FILENO, "and that piece of art is useful"                                    "\" - Dora Korpar, 2015-10-19\n", 41);
+        return (1);
 }
