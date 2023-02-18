ebbc1e9240b3713b49833de2bdde504cd6734e95
Valentine124 <kaluvalentine60@gmail.com>
2023-02-17 20:38:39 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Enrty point
+ *
+ * Return: Always 0 Suces
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z', c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
