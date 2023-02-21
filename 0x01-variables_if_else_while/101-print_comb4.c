11573e6b30769237609b888bd80c2ec75726ac96
Valentine124 <kaluvalentine60@gmail.com>
2023-02-20 16:58:33 -0800

    Task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int f, s, t;
+
+	for (f = 0; f <= 7; f++)
+	{
+
+		for (s = f + 1; s <= 8; s++)
+		{
+
+			for (t = f + 2; t <= 9; t++)
+			{
+				putchar(48 + f);
+				putchar(48 + s);
+				putchar(48 + t);
+
+				if (f == 7 && s == 8 && t == 9)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
