085ceec8ff5e25bfcd68bddb5b5909c38622fa8a
Valentine124 <kaluvalentine60@gmail.com>
2023-02-21 19:40:53 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,19 +2,20 @@
 
 /**
  * _islower - check if  a char is lower case
+ * @c: Int to represent ASCII
  * Return: 1 if true 0 if false
  */
 int _islower(int c)
 {
-	char cs;
+	int n;
 
-	for (cs = 'a'; c <= 'z'; cs++)
+	for (n = 97; c <= 122; n++)
 	{
-		if (c == cs)
+		if (c == n)
 		{
 			return (1);
 		}
 	}
-	
+
 	return (0);
 }
