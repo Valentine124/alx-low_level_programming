06c5b10c84e2dd9fc907a2fe3f33ea390c951345
Valentine124 <kaluvalentine60@gmail.com>
2023-02-21 20:38:34 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - give input absolut value
+ * @: the value in int
+ * Return: absolute value of input
+ */
+int _abs(int ab)
+{
+	return (abs(ab));
+}
