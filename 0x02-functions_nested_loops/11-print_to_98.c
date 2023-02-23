7e36f712844f1ec350e5cdd4add8055af8ec67a9
Valentine124 <kaluvalentine60@gmail.com>
2023-02-22 21:14:43 -0800

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,16 +11,30 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%d, ", n);
-			n--;
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+				n--;
+			}
 		}
 	}
 	else
 	{
 		while (n <= 98)
 		{
-			printf("%d, ", n);
-			n++;
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+				n++;
+			}
 		}
 	}
 }
