9cb0b2b1fa1c68f4652fc71d847e6cbc922a48ec
Valentine124 <kaluvalentine60@gmail.com>
2023-02-24 09:13:47 -0800

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times numbers 0 to 14
+ */
+void more_numbers(void)
+{
+	int c;
+	int n;
+
+	c = 0;
+
+	while (c < 9)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+
+		_putchar('\n');
+		c++;
+	}
+}
