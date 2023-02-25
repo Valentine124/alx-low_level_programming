45db6a227095a0bad181a77f7869666f3cdac67b
Valentine124 <kaluvalentine60@gmail.com>
2023-02-24 17:30:03 -0800

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - draw a square
+ * @size: size of square
+ */
+void print_square(int size)
+{
+	int l;
+	int s;
+
+	if (size > 0)
+	{
+		for (l == 0; l < size; l++)
+		{
+			for (s == 0; s < size; s++)
+				_putchar('#');
+			
+			if (l == size - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
