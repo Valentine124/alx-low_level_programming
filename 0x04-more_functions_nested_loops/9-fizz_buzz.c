2b80ba49268cdd4b899785ae56a1d4626714447a
Valentine124 <kaluvalentine60@gmail.com>
2023-02-24 17:59:39 -0800

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * fizz_buzz - print fizz buzz
+ */
+void fizz_buzz(void)
+{
+	int n;
+	char fizz[];
+	char buzz[];
+
+	fizz = "Fizz";
+	buzz = "Buzz";
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0)
+			printf("%s ", fizz);
+
+		if ((n % 5) == 0)
+			printf("%s ", buzz);
+
+		if ((n % 3) == 0 && (n % 5) == 0)
+			printf("%s%s ", fizz, buzz);
+
+		printf("%d ", n);
+	}
+
+	_putchar('\n');
+}
