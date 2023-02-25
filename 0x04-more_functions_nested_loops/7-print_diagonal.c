d12d2661f511c5276218805a04978e31143500a1
Valentine124 <kaluvalentine60@gmail.com>
2023-02-24 17:02:58 -0800

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: lenght of diagonal
+ */
+void print_diagonal(int n)
+{
+	int l;
+	int s;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+		{
+			for (s = 0; s < l; s++)
+				_putchar(' ');
+			_putchar('\\');
+		}
+	}
+	_putchar('\n');
+}
