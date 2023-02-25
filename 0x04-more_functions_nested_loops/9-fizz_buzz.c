5edb87eecdb06492eb0891492ef6c5c9f52618f1
Valentine124 <kaluvalentine60@gmail.com>
2023-02-24 18:29:05 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,21 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3) == 0)
+		{
 			printf("Fizz");
-
-		if ((n % 5) == 0)
+		}
+		else if ((n % 5) == 0)
+		{
 			printf("Buzz");
-
-		if ((n % 3) == 0 && (n % 5) == 0)
+		}
+		else if ((n % 3) == 0 && (n % 5) == 0)
+		{
 			printf("FizzBuzz");
-
-		printf("%d", n);
-
+		}
+		else
+		{
+	        	printf("%d", n);
+		}
 		if (n == 100)
 			continue;
 		printf(" ");
