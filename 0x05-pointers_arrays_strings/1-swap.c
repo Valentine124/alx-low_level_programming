3d674b982e7b87b37a3cd8cd949d4a72e464595a
Valentine124 <kaluvalentine60@gmail.com>
2023-02-27 14:47:39 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,9 @@
+/**
+ * swap int - given two ints and swap them
+ * @a the first oprand
+ * @b: secod opreand
+ */
+void swap_int(int *a, int *b)
+{
+	_putchar(
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * _strlen - give the lenght of a string
+ * @s: the string
+ * Return: the lenght of the the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+
+	return (len);
+}
