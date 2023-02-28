50d54a1d666a95c65e2e6bd18962fbe138563b65
Valentine124 <kaluvalentine60@gmail.com>
2023-02-27 20:29:29 -0800

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main"
+
+/**
+ * rev_string - reverse a string
+ * @s: the string
+ */
+void rev_string(char *s)
+{
+	int len;
+	int i;
+
+	len = strlen(s);
+	
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
