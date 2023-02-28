09d2de7e84ad4b8ae9df8cb94016d9c88e7cda1f
Valentine124 <kaluvalentine60@gmail.com>
2023-02-28 07:18:41 -0800

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 /**
- * swap int - given two ints and swap them
- * @a the first oprand
+ * swap_int - given two ints and swap them
+ * @a: the first oprand
  * @b: secod opreand
  */
 void swap_int(int *a, int *b)
