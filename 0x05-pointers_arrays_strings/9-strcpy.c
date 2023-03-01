2056688834ac75b9965534e9c64b624f4ebc8174
Valentine124 <kaluvalentine60@gmail.com>
2023-02-28 18:34:28 -0800

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strcpy - copies one pointer to another
+ * @dest: the first pointer
+ * @src: the second pointer
+ * Return: dest
+ */
+char _strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
