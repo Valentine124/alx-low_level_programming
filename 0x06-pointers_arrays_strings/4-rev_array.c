6206e123813b0173914d25df8de0fa4376a60929
Valentine124 <kaluvalentine60@gmail.com>
2023-03-01 19:52:20 -0800

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, h;
+	int i;
+	int h;
 
-	for (i = 0; i < (n / 2); 1++)
+	for (i = 0; i < (n / 2); i++)
 	{
 		h = a[i];
 		a[i] = a[n - i - 1];
-		a[n -i - 1] = h;
+		a[n -i - 1] = hi
 	}
 }
