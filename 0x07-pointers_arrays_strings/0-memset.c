98e6f3f539d708f97859229cd9fc2db2e360efae
Valentine124 <kaluvalentine60@gmail.com>
2023-03-07 18:18:29 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * @s: pointers are memory area
+ * @n: the number of bytes
+ * @b: constant byte
+ *
+ * Return: a pointer
+ */
+char *-memst(char *s, char b unsiggnrd int)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b
+	}
+
+	return (s);
+}
