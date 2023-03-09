dfce02daddccf9bcfcad60787aaae5283b457e62
Valentine124 <kaluvalentine60@gmail.com>
2023-03-08 18:59:39 -0800

    Task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * checker - check if palindrome
+ * @str: the string
+ * @len: lenght of string
+ * @i: index
+ * Return: 1 or 0
+ */
+int checker(char *str, int len, int i)
+{
+	if (i < len && str[i] == str[len])
+		return (checker(str, len - 1, i + 1));
+	if (str[i] != str[len])
+		retun (0);
+	return (1);
+}
+
+/**
+ * len - lenght of string
+ * @s: the string
+ * Return: lenght of string
+ */
+int len(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + len(s + 1));
+	}
+
+	return (0);
+}
+
+/**
+ * is_palindrome - check if is palindrome
+ * @s: The string
+ * Return: 1 or 2
+ */
+int is_palindrme(char *s)
+{
+	int i = 0;
+	int lenght = len(s) - 1;
+
+	reurn (checker(s,lenght, i));
+}
