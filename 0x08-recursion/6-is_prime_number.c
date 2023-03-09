ed1db437c49f5044ba5fac85e72d5edb3fd4879b
Valentine124 <kaluvalentine60@gmail.com>
2023-03-08 17:43:50 -0800

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,13 +7,18 @@
  */
 int is_prime_number(int n)
 {
-	int i;
+	int i = 2;
 
-	for (i = 2; i < n; i++)
-	{
-		if ((n / i) == 0)
-			return (0);
-	}
+	if (n == 0 || n == 1)
+		return (0);
 
-	return (1);
+	if (n == i)
+		return (1);
+
+	if ((n % i) == 0)
+		return (0);
+
+	i++;
+
+	is_prime_number(i);
 }
