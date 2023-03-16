f8c7ebfffc5e204e2b0b433ad0544b351cfb86f7
Valentine124 <kaluvalentine60@gmail.com>
2023-03-16 14:07:22 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory using malloc
+ *
+ * @b: size of memory
+ *
+ * Return: nothing
+ */
+void malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+}
