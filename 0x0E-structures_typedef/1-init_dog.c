fdd47acb5a9bd8a28b6ea6a0aa9638cd64308e79
Valentine124 <kaluvalentine60@gmail.com>
2023-03-21 19:46:41 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+/**
+ * init_dog - initialize dog
+ *
+ * @d: d of type dog
+ *
+ * @name: name of type char
+ *
+ * @age: age is a float
+ *
+ * @owner: owner is a char
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
