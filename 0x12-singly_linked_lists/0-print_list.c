7a37f3a72a6042053a6c02150b8dffe67ddb5473
Valentine124 <kaluvalentine60@gmail.com>
2023-03-31 12:38:42 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdio.h>
 #include "lists.h"
 
 void print_node(const list_t *n);
@@ -19,26 +20,13 @@ size_t print_list(const list_t *h)
 		return (0);
 
 	do {
-		print_node(temp);
+		printf("");
 
 		temp = temp->next;
 		n++;
 	} while (temp != NULL);
 
 	return (n);
-}
-
-/**
- * print_str - print string
- * @s: the string to be printed
- */
-void print_str(char *s)
-{
-	while (*s != '\0')
-	{
-		_putchar(*s);
-		s++;
-	}
 }
 
 /**
@@ -49,20 +37,10 @@ void print_node(const list_t *n)
 {
 	if (n->str == NULL)
 	{
-		_putchar('[');
-		_putchar('0' + n->len);
-		_putchar(']');
-		_putchar(' ');
-		print_str("(nil)");
-		_putchar('\n');
+		printf("[%d] (nil)", n->len);
 	}
 	else
 	{
-		_putchar('[');
-		_putchar('0' + n->len);
-		_putchar(']');
-		_putchar(' ');
-		print_str(n->str);
-		_putchar('\n');
+		printf("[%d] %s", n->len, n->str);
 	}
 }
