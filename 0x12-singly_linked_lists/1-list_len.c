d2124eca2e367e164bf689399b450c9ae704e7eb
Valentine124 <kaluvalentine60@gmail.com>
2023-03-31 13:06:25 -0700

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * list_len - returns length of linked list
+ * @h: the first element of the list
+ * Return: numbers of element in the list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t temp = h;
+	size_t length = 0;
+
+	if (temp != NULL)
+	{
+		do {
+			length++;
+			temp = temp->next;
+
+		} wile (temp != NULL)
+	}
+
+	return (length);
+}
