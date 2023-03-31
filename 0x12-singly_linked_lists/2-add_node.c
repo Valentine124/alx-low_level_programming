f3037a312c4a7c493618be9b0805bafa09435c44
Valentine124 <kaluvalentine60@gmail.com>
2023-03-31 14:37:19 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,19 +4,34 @@
 
 /**
  * add_node - add a new node to the begening of the list
- * @h: the first node
+ * @head: the first node
  * @str: string to be duplicated
  * Return: address of the new node or NULL
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	char *dup;
+	unsigned int len;
+
 	list_t *new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 
+	dup = strdup(str);
+	
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len]; len++)
+		;
+
 	new->next = *head;
-	new->str = strdup(str);
+	new->str = dup;
+	new->len = len;
 	*head = new;
 
 	return (new);
