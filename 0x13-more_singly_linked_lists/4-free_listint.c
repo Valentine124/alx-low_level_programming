ec266f02d61996b58abc4bdff1dc583de63c86ae
Valentine124 <kaluvalentine60@gmail.com>
2023-04-03 20:57:48 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - free a linked list
+ * @head: the first node
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next == NULL)
+		free(head);
+	else
+		free_listint(head->next);
+}
