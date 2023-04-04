ba724cfd0f8894cdcb0d3621bfa1ac93dcbc2a1a
Valentine124 <kaluvalentine60@gmail.com>
2023-04-04 07:32:55 -0700

    Task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - remove the first node
+ * @head: the first node
+ * Return: the node removed
+ */
+int pop_listint(listint_t **head)
+{
+	int n = (*head)->n;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (n);
+}
