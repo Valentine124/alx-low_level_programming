01f32049db6498ba81aee57e2360c1eac0cc06bc
Valentine124 <kaluvalentine60@gmail.com>
2023-04-10 06:07:14 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		w = write(f, text_content, strlen(text_content));
 	
-	if (f == -1 || w = -1)
+	if (f == -1 || w == -1)
 		return (-1);
 
 	close(f);
