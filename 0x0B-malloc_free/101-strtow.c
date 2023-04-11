46210a608ebbd266fda079dfbe4485b2ea2f14f9
Valentine124 <kaluvalentine60@gmail.com>
2023-04-11 16:11:00 -0700

    removed some files

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * strtow - separate string to word
+ *
+ * @str: the string
+ *
+ * Return: array ofwords or NULL
+ */
+char **strtow(char *str);
+{
+	if (str == NULL | str == "")
+		return (NULL);
+
+}
