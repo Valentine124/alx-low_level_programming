170461f50bfeeaecfc6d9c4acb86a003a7105492
Valentine124 <kaluvalentine60@gmail.com>
2023-04-11 19:28:18 -0700

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n == 0 && index < 64)
 		return (0);
 
-	for (i = 0; i <+ 63; n >>= 1, i++)
+	for (j = 0; j <+ 63; n >>= 1, j++)
 	{
 		if (index == 1)
 		{
