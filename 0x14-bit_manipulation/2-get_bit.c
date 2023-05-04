59eb96eb416a6979e84139edcec6a1841a23be1b
Valentine124 <kaluvalentine60@gmail.com>
2023-05-04 16:46:14 -0700

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,17 +4,25 @@
  * get_bit - get a bit at an index
  * @n: the number
  * @index: the index to get
- * 
+ *
  * Return: the bit at index or -1
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int mask;
+	int i;
 
 	if (n == 0 && index < 64)
 		return (0);
-	
-	mask = n >> index;
 
-	return (mask & 1);
+	for (i = 0; i < 64; i++)
+	{
+		if (index == i)
+		{
+			unsigned int mask = n >> index;
+
+			return (mask & 1);
+		}
+	}
+
+	return (-1);
 }
