9ea9e27b35ec80d41486b67582756c69ace713f0
Valentine124 <kaluvalentine60@gmail.com>
2023-05-04 13:21:04 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,7 +22,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		int n, l = len;
 
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		if (b[i] == '0')
 			n = 0;
