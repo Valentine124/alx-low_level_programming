59e254cca9b58ec62fe9727f6b4fd2dab3f789cd
Valentine124 <kaluvalentine60@gmail.com>
2023-06-05 13:27:56 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - program to print sum of fib even values
+ *
+ * Return: Success 0
+ */
+int main(void)
+{
+	long int first = 1, second = 2, next = 0;
+	long int sum = second;
+
+	while (next <= 4000000)
+	{
+		next = first + second;
+		fist = second;
+		second = next;
+
+		if (next % 2 == 0)
+			sum += next;
+	}
+
+	printf("%ld\n", sum);
+
+	return (0);
+}
