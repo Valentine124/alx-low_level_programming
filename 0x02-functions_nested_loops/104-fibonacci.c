e7944513d48b80e19986fe39dc222d4e1e8450fc
Valentine124 <kaluvalentine60@gmail.com>
2023-06-05 13:50:35 -0700

    Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program to first 98 fibonacci number
+ *
+ * Return: Success 0
+ */
+int main(void)
+{
+	unsigned long first = 1, second = 2, next, i = 0;
+
+	printf("%lu, %lu, ", first, second);
+
+	while (i < 96)
+	{
+		next = first + second;
+		first = second;
+		second = next;
+
+		if (i == 95)
+			printf("%lu", next);
+		else
+			printf("%lu, ", next);
+
+		i++;
+	}
+	printf("\n");
+}
