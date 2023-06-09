37850b36eda09cea0456189c681d01bd61928c00
Valentine124 <kaluvalentine60@gmail.com>
2023-06-09 08:18:48 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,41 @@
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * _atoi - print int
- * @s: the parameter
- * Return: int value of tr chsrt
+ * _atoi - convert ascii to int
+ * @s: thestring
+ * Return: integer representation
  */
 int _atoi(char *s)
 {
-	return (atoi(s));
+	int c, n, num = 0, sign = 1;
+
+	if (s == NULL)
+		return (num);
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == '-')
+			sign *= -1;
+		else if (s[c] == '+')
+			sign *= +1;
+		else
+		{
+			if (s[c] >= '0' && s[c] <= '9')
+			{
+				for (n = 0; n < 9; n++)
+				{
+					if ('0' + n == s[c])
+					{
+						num = (num * 10) + n;
+						break;
+					}
+				}
+
+				if (!(s[c + 1] >= '0' && s[c + 1] <= '9'))
+					return (sign * num);
+			}
+		}
+	}
+
+	return (sign * num);
 }
