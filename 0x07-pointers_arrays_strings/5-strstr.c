a7d8badca1016b20a9e1861a9cd89f25a4363804
Valentine124 <kaluvalentine60@gmail.com>
2023-06-22 09:31:04 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,4 +6,28 @@
  * @needle: the substring
  * Return: Pointer to the begining of the located substring or NULL
  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	char *sub = NULL;
 
+	for (i = 0; haystack[i]; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			sub = &haystack[i];
+			for (j = 1; needle[j]; i++)
+			{
+				if (haystack[i + 1] != needle[j])
+				{
+					sub = NULL;
+					break;
+				}
+			}
+
+			if (needle[j] == '\0')
+				return (sub);
+		}
+	}
+	return (sub);
+}
