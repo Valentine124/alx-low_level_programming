7de4d6cae85fa7a7d1cfe7a02645d61b3fefb896
valentine <kaluvalentine60@gmail.com>
2023-07-06 17:32:05 -0700

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (count < index)
+	while (temp != NULL && count < index)
 	{
 		prev = temp;
 		temp = temp->next;
