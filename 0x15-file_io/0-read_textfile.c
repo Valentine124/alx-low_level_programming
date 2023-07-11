e8d747e5f112816aef750fadc055512f62b73d81
valentine <kaluvalentine60@gmail.com>
2023-07-10 19:40:27 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text file and print to stdout
+ * @filename: name of the file
+ * @letters: numger of letter to be read
+ *
+ * Return: number of byte read 0r 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, r, w;
+	char *buff = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+		return (0);
+
+	if (buff == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	r = read(fd, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
+
+	if (fd == -1 | r == -1 | w == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	free(buff);
+	close(fd);
+
+	return (r);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* All functions */
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
