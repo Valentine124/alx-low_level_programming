011d5521b509536238f3989e1327f14426bed940
valentine <kaluvalentine60@gmail.com>
2023-08-17 07:18:10 -0700

    Task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all element in a linked list
+ * @h: the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_dlidtint(const distint_t *h)
+{
+	distint_t *temp;
+	size_t len = 0;
+
+	if (h == NULL)
+		return (0);
+
+	temp = h;
+
+	while (temp)
+	{
+		len += 1;
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+
+	return (len);
+}
