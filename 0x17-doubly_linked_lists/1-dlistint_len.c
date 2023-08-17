b0bd69aaa804451b0019c5a0617c76c89becad60
valentine <kaluvalentine60@gmail.com>
2023-08-17 08:19:55 -0700

    Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - get the number of element in a linked list
+ * @h: the list
+ *
+ * Return: number of element
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	size_t len = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (temp)
+	{
+		len += 1;
+		temp = temp->next;
+	}
+
+	return (len);
+}
