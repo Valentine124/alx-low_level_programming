d295c5329bc896860e7feeec2e02d1e6d53f9347
valentine <kaluvalentine60@gmail.com>
2023-08-21 21:32:52 -0700

    task 2

---
+++ 0x18-dynamic_libraries/cpy.c
@@ -0,0 +1,61 @@
+#include "cpy.h"
+
+/**
+ * add - add two numbers
+ * @a: the first
+ * @b: the second
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: the first
+ * @b: the second
+ *
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: the first
+ * @b: the second
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two numbers
+ * @a: the first
+ * @b: the second
+ *
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - divide two numbers and return the remender
+ * @a: the first
+ * @b: the second
+ *
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
