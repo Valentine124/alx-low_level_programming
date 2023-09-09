4fc81c2a31eb458f7711f5b9090de0253425f4b5
valentine <kaluvalentine60@gmail.com>
2023-09-08 19:18:42 -0700

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	hash_node_t *new_arr;
+	hash_node_t **new_arr;
+	unsigned long int i;
 
 	if (size <= 0)
 		return (NULL);
@@ -19,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table == NULL)
 		return (NULL);
 
-	new_arr = malloc(sizeof(*new_arr) * size);
+	new_arr = malloc(sizeof(new_arr) * size);
 	if (new_arr == NULL)
 		return (NULL);
 
@@ -27,7 +28,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		new_arr[i] = NULL;
 
 	new_table->size = size;
-	new_table->array = &new_arr;
+	new_table->array = new_arr;
 
 	return (new_table);
 }
