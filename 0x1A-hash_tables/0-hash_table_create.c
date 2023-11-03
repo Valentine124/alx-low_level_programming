1317846f6abcfda7748cf2e380da4b2863591a53
valentine124 <kaluvalentine60@gmail.com>
2023-11-02 23:44:28 -0700

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,8 +13,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = malloc(sizeof(*table));
+	if (table == NULL)
+		return (NULL);
 	arr = malloc(sizeof(*arr) * size);
-	if (arr == NULL || table == NULL)
+	if (arr == NULL)
 		return (NULL);
 
 	/* Assign NULL to all spaces in arr */
