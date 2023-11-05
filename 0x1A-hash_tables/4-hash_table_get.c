18c3f3910dc30676bc6b9af3d0e5a93b57f67862
valentine124 <kaluvalentine60@gmail.com>
2023-11-05 09:14:02 -0800

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *head, *temp;
 
+	if (ht == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *) key, ht->size);
 
 	head = ht->array[index];
