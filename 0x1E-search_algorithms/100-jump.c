c77097cbf558198ead3254cdbb3771e56b439ab0
valentine124 <valentinekalu60@gmail.com>
2024-05-08 17:01:14 -0700

    Task 7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,26 +17,30 @@ int jump_search(int *array, size_t size, int value)
 		return (-1);
 
 	jump = sqrt(size);
-
 	for (i = 0; i < size; i += jump)
 	{
 		if (array[i] >= value)
 		{
+			if (i == size - 1)
+			{
+				printf("Value found between indexes [%ld] and [%ld]\n", i, i + jump);
+				break;
+			}
 			printf("Value found between indexes [%ld] and [%ld]\n", i - jump, i);
 			break;
 		}
-
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (i >= size - 1)
 		{
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			printf("Value found between indexes [%ld] and [%ld]\n", i, i + jump);
 			return (-1);
 		}
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
 
 	for (j = i - jump; j <= i; j++)
 	{
+		if (i == size - 1)
+			j = i;
 		if (array[j] == value)
 		{
 			printf("Value checked array[%ld] = [%d]\n", j, array[j]);
@@ -44,8 +48,6 @@ int jump_search(int *array, size_t size, int value)
 		}
 		else
 			printf("Value checked array[%ld] = [%d]\n", j, array[j]);
-
 	}
-
 	return (-1);
 }
