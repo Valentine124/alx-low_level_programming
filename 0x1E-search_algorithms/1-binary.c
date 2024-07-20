9383060556fb16a5ac29d901761a03a1a8c1767f
Valentine124 <kaluvalentine60@gmail.com>
2024-07-20 10:32:23 -0700

    Task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,33 +8,33 @@
  * @value: The value to search for
  * Return: The index where value is located or -1
  */
- int binary_search(int *array, size_t size, int value)
- {
-	 size_t mid, first, last;
-	 
-	 if (array == NULL)
-		 return (-1);
-	 
-	 first = 0;
-	 last = size - 1;
-	 
-	 while (first <= last)
-	 {
-		 printf("Searching in array: ");
-		 print_arr(array, first, last);
+int binary_search(int *array, size_t size, int value)
+{
+	size_t mid, first, last;
 
-		 mid = (first + last) / 2;
+	if (array == NULL)
+		return (-1);
 
-		 if (value == array[mid])
-			 return (mid);
-		 else if (value >= array[mid + 1])
-			 first = mid + 1;
-		 else if (value <= array[mid - 1])
-			 last = mid - 1;
-	 }
+	first = 0;
+	last = size - 1;
 
-	 return (-1);
- }
+	while (first <= last)
+	{
+		printf("Searching in array: ");
+		print_arr(array, first, last);
+
+		mid = (first + last) / 2;
+
+		if (value == array[mid])
+			return (mid);
+		else if (value >= array[mid + 1])
+			first = mid + 1;
+		else if (value <= array[mid - 1])
+			last = mid - 1;
+	}
+
+	return (-1);
+}
 
 /**
  * print_arr - prints the values of an array
